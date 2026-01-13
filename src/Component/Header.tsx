9711ae39be2e7d96cde38d8231752635b6ea2317
Mahesh kumar <vickymandal542@gmail.com>
2026-01-14 05:15:43 +0530

    header updated

--- src/Component/Header.tsx
+++ src/Component/Header.tsx
@@ -1,20 +1,25 @@
 import React from "react";
 import { NetflixLogo } from "../utils/constants";
+import LanguageSelector from "./LanguageSelector";
 
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 w-full bg-gradient-to-b from-black px-8 py-4 flex items-center justify-between">
-      
+    <header className="fixed top-0 left-0 z-50 w-full bg-gradient-to-b from-black px-8 py-4 flex items-center justify-between">
+      {/* Left: Logo */}
       <img
         className="w-44 cursor-pointer"
         src={NetflixLogo}
         alt="netflix-logo"
       />
 
-   
-      <button className="rounded-md bg-red-600 px-6 py-2 text-white font-semibold hover:bg-red-700 transition">
-        Sign Out
-      </button>
+      {/* Right: Language selector + Sign Out */}
+      <div className="flex items-center gap-4">
+        <LanguageSelector />
+
+        <button className="rounded-md bg-red-600 px-6 py-2 text-white font-semibold hover:bg-red-700 transition">
+          Sign Out
+        </button>
+      </div>
     </header>
   );
 };

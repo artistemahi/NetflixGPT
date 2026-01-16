ee0925b20c9222941ac4f079cc196f95a06569cc
Mahesh kumar <vickymandal542@gmail.com>
2026-01-16 19:45:53 +0530

    store setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Body from "./Component/Body";
 import Browse from "./Component/Browse";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore"
+
+const AppLayout=()=>{
+  return (
+    <Provider store={appStore}>
+    <Body/>
+    </Provider>
+  )
+}
 
 function App(): React.ReactElement {
   const appRoute = createBrowserRouter([
     {
       path: "/",
-      element: <Body />,
+      element: <AppLayout />,
     },
     {
       path: "/browse",

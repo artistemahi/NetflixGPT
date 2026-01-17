c878e77441f4cf1aabad467311db4eeeb45118b4
Mahesh kumar <vickymandal542@gmail.com>
2026-01-18 03:43:28 +0530

    video aspect correct

--- src/Component/VideoBackground.tsx
+++ src/Component/VideoBackground.tsx
@@ -5,10 +5,13 @@ const VideoBackground = ({ movieId }) => {
   const trailerVideo = useSelector((store: any) => store.movie?.trailerVideo);
   useVideos(movieId);
   return (
-    <div className="w-screen">
+    <div className="absolute inset-0 -z-10 overflow-hidden">
       <iframe
-        className="w-screen aspect-video"
-        src={"https://www.youtube.com/embed/"+trailerVideo?.key}
+        className="absolute top-1/2 left-1/2 
+             w-[120vw] h-[120vh]
+             -translate-x-1/2 -translate-y-1/2
+             scale-150"
+        src={"https://www.youtube.com/embed/"+trailerVideo?.key+"?&autoplay=1&mute=1" }
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
--- src/Component/VideoTitle.tsx
+++ src/Component/VideoTitle.tsx
@@ -1,15 +1,15 @@
 const VideoTitle = ({ title, Overview }) => {
   return (
-    <div>
-      <div className="my-72 mx-9 absolute" >
+      <div className=" w-screen absolute pt-[10%] px-9 aspect-video bg-gradient-to-r from-black  text-white" >
+        <div className=" px-4 ">
         <h1 className="my-4 font-bold , text-4xl">{title}</h1>
-        <p>{Overview}</p>
+        <p className="w-1/4 text-white">{Overview}</p>
         <div>
-            <button className="bg-white font-bold text-2xl rounded-sm px-3 py-0.5">► Play</button>
-            <button className="text-white  text-2xl  font-bold bg-gray-500 opacity-50 rounded-sm px-3 py-0.5">ⓘ More Info</button>
+            <button className="bg-stone-50 font-bold text-black  hover:bg-stone-300 text-2xl mt-4 focus:border-black border-[1px] rounded-md px-3 py-0.5">► Play </button>
+            <button className="text-white text-2xl mt-4 mx-2 bg-opacity-50 hover:bg-gray-300 focus:border-white border-[1px] font-bold bg-gray-600  rounded-md px-3 py-0.5">ⓘ More Info </button>
         </div>
       </div>
-    </div>
+      </div>
   );
 };
 
